7fde9c213b01d2313a691665ac614d39fa53a384
= <7rubengarcia7@gmail.com>
2019-04-02 12:21:20 -0700

    removed to label hack

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,28 +4,67 @@
 // navigation is removed and replaced with the hamburger icon.
 
 (function() {
-  const navLinks = document.querySelectorAll(".nav__list__item");
+  // get a reference to the li's that contains the necessary
+  // data atribute.
+  const navLinks = document.querySelectorAll('.nav__item');
 
+  // when the user clicks on a link
+  // create a smooth effect that takes the user to the appropriate location.
   navLinks.forEach(function(link) {
-    link.addEventListener("click", function(e) {
-      const target = link.getAttribute("data-destination");
+    link.addEventListener('click', function() {
+      // grab the value of data-description attribute
+      const target = link.getAttribute('data-destination');
+      // find the DOM element.
       const targetDestination = document.getElementById(target);
+      // get the pixels of the top of the target element.
       const scrollTop = targetDestination.getBoundingClientRect().top;
 
+      // scroll to the target element.
       window.scrollBy({
         top: scrollTop,
-        behavior: "smooth"
+        behavior: 'smooth',
       });
     });
   });
 
   // remove mobile nav after user has clicked on link.
   if (window.innerWidth <= 900) {
-    navLinks.forEach(function(link) {
-      link.addEventListener("click", function() {
-        const checkbox = document.getElementById("checkbox");
+    // grab the button with the hamburger icon.
+    const button = document.querySelector('.button--open');
+    // grab the hamburger icon
+    const icon = button.firstElementChild;
+    const navList = document.querySelector('.nav__list');
 
-        checkbox.checked = false;
+    // when the user clicks on the hamburger icon
+    // slide the menu from the right.
+    button.addEventListener('click', function() {
+      // if hamburger icon is present.
+      if (this.classList.contains('button--open')) {
+        button.classList.remove('button--open');
+        button.classList.add('button--close');
+
+        // remove the hamburger icon class
+        icon.classList.remove('fa-bars');
+        // add the x icon class
+        icon.classList.add('fa-times');
+
+        navList.classList.add('nav__list--mobile');
+      } else {
+        button.classList.remove('button--close');
+        button.classList.add('button--open');
+
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+
+        navList.classList.remove('nav__list--mobile');
+      }
+    });
+
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function() {
+        navList.classList.remove('nav__list--mobile');
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
       });
     });
   }
